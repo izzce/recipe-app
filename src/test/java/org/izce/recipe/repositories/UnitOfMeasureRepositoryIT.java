6e690c6880b4badd8ec7dd48616b98798247194c
izzce <izzetcelik@gmail.com>
2022-01-12 21:32:27 +0300

    Add Jacoco for code coverage check. Closes #30

--- src/test/java/org/izce/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/org/izce/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -8,13 +8,13 @@ import org.izce.recipe.model.UnitOfMeasure;
 import org.izce.recipe.service.StorageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class) 
-@DataJpaTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+//
+//@DataJpaTest
+@SpringBootTest
+@AutoConfigureTestDatabase
 public class UnitOfMeasureRepositoryIT {
 	
 	@Autowired
